4dcfe07665ca96bd324e76817890811bf87905b1
Meggan <meggdo@gmail.com>
2018-05-29 17:16:55 -0400

    18ms runtime yay!

--- 657 - Judge Route Circle.cpp
+++ 657 - Judge Route Circle.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
     int x=0;
-    int y=0;
-    std::string parsedChar ="";
-        for ( std::string::iterator it = moves.begin(); it != moves.end(); ++it){
-            parsedChar = *it;
-            
-            //std::cout << parsedChar <<std::endl;
-            if(parsedChar == "U"){
-                y+=1;
-            }
-            else if(parsedChar == "D"){
-                y-=1;
-            }
-            else if(parsedChar == "L"){
+        for(int i=0;i<moves.size();i++)
+        {
+            if(moves[i]=='U'||moves[i]=='R'){
                 x+=1;
             }
-            else if(parsedChar == "R"){
+            else {
                 x-=1;
             }
         }
-
-        return x == 0 && y == 0;
+        return x == 0 ;
     }
 };
